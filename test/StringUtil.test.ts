eece068c924db791d9c770e0fc34276833cfb94e
KD0NKS <andrew.m.fairhurst@gmail.com>
2019-11-03 15:44:10 -0600

    Allowing null or undefined string on null or whitespace check.

--- test/StringUtil.test.ts
+++ test/StringUtil.test.ts
@@ -4,8 +4,16 @@ import { StringUtil } from '../src/util/StringUtil';
 const assert = chai.assert;
 
 describe('StringUtil Tests', () => {
+    it("Should return true when there is no parameter.", () => {
+        assert.equal(true, StringUtil.IsNullOrWhiteSpace());
+    });
+
     it("Should return true when value is null.", () => {
         assert.equal(true, StringUtil.IsNullOrWhiteSpace(null));
+    });
+
+    it("Should return true when value is undefined.", () => {
+        assert.equal(true, StringUtil.IsNullOrWhiteSpace(undefined));
     });
 
     it("Should return true when value is empty string.", () => {
